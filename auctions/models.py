5207300298c8f0fea94421da063939fdb740154b
koleks92 <koleks92@gmail.com>
2023-03-30 20:52:35 +0200

    Models

--- auctions/models.py
+++ auctions/models.py
@@ -2,5 +2,40 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+
 class User(AbstractUser):
+    pass
+
+class Listing(models.Model):
+    CATEGORIES = [
+    ('electornics', 'Electronics'),
+    ('motors', 'Motors'),
+    ('fashion', 'Fashion'),
+    ('collectibles', 'Collectibles and Art'),
+    ('sports', 'Sports'),
+    ('health', 'Health and Beauty'),
+    ('industrial', 'Industrial Equipment'),
+    ('home', 'Home and Garden')
+]
+
+    name = models.CharField(max_length=64)                                                  #name
+    description = models.CharField(max_length=640, null=True)                                          #description
+    price = models.DecimalField(max_digits=20, decimal_places=2)                            #price
+    date = models.DateTimeField(auto_now_add=True)                                          #date creation
+    image = models.ImageField()                                                             #image
+    category = models.CharField(max_length=64, choices=CATEGORIES)                          #category
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")           #user
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)                                            # user
+    date = models.DateTimeField(auto_now=True)                                                          # date bid
+    bid = models.DecimalField(max_digits=20, decimal_places=2, default=0)                               # user bid
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listing", default=0)   # listing
+    pass
+
+class Comment(models.Model):
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comment", default=0)      # listing                      # item
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="User", default=0)            # user
+    date = models.DateTimeField(auto_now=True)                                                          # date comment
+    comment = models.CharField(max_length=128, default="")                                              # comments
     pass
--- auctions/views.py
+++ auctions/views.py
@@ -3,6 +3,8 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 from .models import User
 
@@ -61,3 +63,7 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+    
+@login_required
+def create(request):
+    return render(request, "auctions/create.html")
